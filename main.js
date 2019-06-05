77ccdb37db81fa8ad1818aadf7d73999e153c686
Frari <francescoriccardi86@gmail.com>
2019-06-05 18:35:35 +0200

    problema con l'aggiornamento dei grafici

--- main.js
+++ main.js
@@ -32,60 +32,73 @@ $(document).ready(function(){
   var labels = [];
   var array = [];
 
-// chiamata ajax per i ricavi mensili
-  $.ajax({
-    'url':url_base,
-    'method':'GET',
-    'success':function(data){
+  stampaGrafici(url_base, mesi, labels, array);
+  function stampaGrafici(url_base, mesi, labels, array){
+    // chiamata ajax per i ricavi mensili
+      $.ajax({
+        'url':url_base,
+        'method':'GET',
+        'success':function(data){
 
-      for (var i = 0; i < data.length; i++) {
-// creo una variabile nella quale do il formato delle date utilizzando
-// monet e pendo solo i mesi delle varie date
-        var mese = moment(data[i].date, 'DD/MM/YYYY').format('M');
-// ciclo for in con il quale sommo e assegno i profitti ai vari
-// mesi dell'oggetto 'mesi'.
-        for(prop_mese in mesi){
-          if(prop_mese == mese){
-            mesi[prop_mese] += data[i].amount;
+          for (var i = 0; i < data.length; i++) {
+    // creo una variabile nella quale do il formato delle date utilizzando
+    // monet e pendo solo i mesi delle varie date
+            var mese = moment(data[i].date, 'DD/MM/YYYY').format('M');
+    // ciclo for in con il quale sommo e assegno i profitti ai vari
+    // mesi dell'oggetto 'mesi'.
+            for(prop_mese in mesi){
+              if(prop_mese == mese){
+                mesi[prop_mese] += data[i].amount;
+              }
+            }
+
           }
+
+    // manipolazione dati per il secondo grafico
+          for (var i = 0; i < data.length; i++) {
+            var venditori = data[i].salesman;
+            if(!labels.includes(venditori)){
+              labels.push(venditori);
+            }
+            // var guadagni = data[i].amount;
+          }
+    // ciclo sull'array dei 4 venditori
+          for (var i = 0; i < labels.length; i++) {
+    // variabile d'appoggio che identifica il venditore ciclato nel momento
+            var venditore_corrente = labels[i];
+    // variabile per andare a sommare i guadagni del venditore ciclato
+            var somma_prof_sing_vend = 0;
+    // ciclo dell'array iniziale
+           for (var j = 0; j < data.length; j++) {
+             var data_corrente = data[j];
+    // per ogni vendita controllo de corrisponde al veditore corrente
+    // se si sommo
+             if(data_corrente.salesman == venditore_corrente){
+               somma_prof_sing_vend += data_corrente.amount;
+             }
+           }
+    // faccio il push delle somme profitti per ogni veditore
+           array.push(somma_prof_sing_vend);
+
+          }
+    // ciclo l'array labels per popolare la selection dei venditori
+          for (var i = 0; i < labels.length; i++) {
+            $('.sel_venditori').append('<option>'+labels[i]+'</option>')
+          }
+    // ciclo le chiavi dell'array mesi per popolare la selection dei mesi
+          for (var i = 0; i < Object.keys(mesi).length; i++) {
+            $('.sel_mesi_anno').append('<option>'+Object.keys(mesi)[i]+'</option>')
+          }
+          create_graph(Object.keys(mesi), Object.values(mesi));
+          create_pie(labels, array);
+        },
+        'error':function(){
+          alert(errore);
         }
+      })
+  }
 
-      }
 
-// manipolazione dati per il secondo grafico
-      for (var i = 0; i < data.length; i++) {
-        var venditori = data[i].salesman;
-        if(!labels.includes(venditori)){
-          labels.push(venditori);
-        }
-        // var guadagni = data[i].amount;
-      }
-// ciclo sull'array dei 4 venditori
-      for (var i = 0; i < labels.length; i++) {
-// variabile d'appoggio che identifica il venditore ciclato nel momento
-        var venditore_corrente = labels[i];
-// variabile per andare a sommare i guadagni del venditore ciclato
-        var somma_prof_sing_vend = 0;
-// ciclo dell'array iniziale
-       for (var j = 0; j < data.length; j++) {
-         var data_corrente = data[j];
-// per ogni vendita controllo de corrisponde al veditore corrente
-// se si sommo
-         if(data_corrente.salesman == venditore_corrente){
-           somma_prof_sing_vend += data_corrente.amount;
-         }
-       }
-// faccio il push delle somme profitti per ogni veditore
-       array.push(somma_prof_sing_vend);
-
-      }
-      create_graph(Object.keys(mesi), Object.values(mesi));
-      create_pie(labels, array);
-    },
-    'error':function(){
-      alert(errore);
-    }
-  })
 // funzione che mi disegna il primo grafico
   function create_graph(chiavi, valori){
     var ctx = document.getElementById('myChart').getContext('2d');
@@ -131,27 +144,33 @@ $(document).ready(function(){
     });
   };
   // chiamata ajax per inserimento nuove vendite
-  $('#but_inserimento').click(function(){
+  $(document).on('click','#but_inserimento',function(){
 
     var venditore_selezionato = $('.sel_venditori').val();
     var mese_selezionato = $('.sel_mesi_anno').val();
     var importo_inserito = $('#input_inserimento').val();
+    var mese_moment = '01/'+moment(mese_selezionato, 'M').format('MM')+'/2017';
+
+    console.log(venditore_selezionato);
+    console.log(mese_moment);
+    console.log(importo_inserito);
+    $.ajax({
+      'url':url_base,
+      'method':'POST',
+      'data': JSON.stringify({
+        'salesman':venditore_selezionato,
+        'date': mese_moment,
+        'amount':parseInt(importo_inserito)
+      }),
+      'contentType':'application/json',
+      'success':function(data){
+        stampaGrafici(url_base, mesi, labels, array);
+      },
+      'error':function(){
+        alert(errore);
+      }
+    })
   })
-  $.ajax({
-    'url':url_base,
-    'method':'POST',
-    'data':{
-      'salesman':venditore_selezionato,
-      'date': mese_selezionato,
-      'amount':importo_inserito
-    },
-    'success':function(data){
-      stampaLista();
-      // $('.list').append('<div>'+ data.text +'</div>');
-    },
-    'error':function(){
-      alert(errore);
-    }
-  })
+
 
 });
